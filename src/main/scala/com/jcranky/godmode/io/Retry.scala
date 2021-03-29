b327b53323fb9868f7681bda053deb0211617c8a
Scala Steward <me@scala-steward.org>
2021-03-29 09:00:59 +0200

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=series/3.x
    See https://github.com/typelevel/cats-effect/blob/ce3-migration-guide/migration-guide.md for details

--- src/main/scala/com/jcranky/godmode/io/Retry.scala
+++ src/main/scala/com/jcranky/godmode/io/Retry.scala
@@ -1,16 +1,17 @@
 package com.jcranky.godmode.io
 
-import cats.effect.{Sync, Timer}
+import cats.effect.Sync
 import cats.syntax.applicativeError._
 import cats.syntax.apply._
 
 import scala.concurrent.duration._
+import cats.effect.Temporal
 
 object Retry {
 
   implicit class RetryOps[F[_], A](fa: F[A]) {
 
-    def retry(delay: FiniteDuration = 1.second, maxRetries: Int = 7)(implicit timer: Timer[F], F: Sync[F]): F[A] =
+    def retry(delay: FiniteDuration = 1.second, maxRetries: Int = 7)(implicit timer: Temporal[F], F: Sync[F]): F[A] =
       fa.handleErrorWith { error =>
         if (maxRetries > 0)
           F.delay(println(s"F[A] failed with [$error], retrying")) *> timer.sleep(delay) *> fa.retry(delay * 2, maxRetries - 1)
